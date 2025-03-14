67aee165ef8d455e8b018da38e209b437423bffb
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2025-03-14 14:53:15 -0700

    Added alternate base path configuration

---
+++ tests/Controllers/FileControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Controllers;
+
+use Tests\TestCase;
+
+/** @covers \App\Controllers\FileController */
+class FileControllerTest extends TestCase
+{
+    public function test_it_can_do_something(): void
+    {
+        // ...
+
+        $this->markTestIncomplete();
+    }
+}
